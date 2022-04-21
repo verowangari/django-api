c2211e833b49a6c7c054a555d02413c02be9774e
verowangari <verowangari34@gmail.com>
2022-04-21 18:32:36 +0300

    import api_view and response

--- App/views.py
+++ App/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
 # Create your views here.
 def apiOverview(request):
     return JsonResponse("API BASE POINT", safe=False)
--- todo/urls.py
+++ todo/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('App/', include('App.urls'))
+    path('api/', include('App.urls'))
 ]
