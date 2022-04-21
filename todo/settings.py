ed347de1addd85e57fa2a6965b319bf8aaca1a06
verowangari <verowangari34@gmail.com>
2022-04-21 21:46:59 +0300

    create a urls file and templates folder

--- todo/settings.py
+++ todo/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'App',
-    'frontend'
+    'frontend',
     'rest_framework',
 ]
 
--- todo/urls.py
+++ todo/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('App.urls'))
+    path('api/', include('App.urls')),
+    path('', include('frontend.urls'))
 ]
