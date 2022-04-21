49c6974fb9e3875c1ef7ce21168aee17eea9bac7
verowangari <verowangari34@gmail.com>
2022-04-21 20:14:58 +0300

    create superuser

--- App/serializers.py
+++ App/serializers.py
@@ -2,6 +2,6 @@ from rest_framework import serializers
 from .models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
-    class meta:
-        model=Task
-        fields='__all__'
+	class Meta:
+		model = Task
+		fields ='__all__'
--- App/views.py
+++ App/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from yaml import serialize
+ 
 from .serializers import TaskSerializer
 from .models import Task
 
