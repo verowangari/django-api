9143b7f33325bb9b32e1f866eef7d82cd9ac9ae9
verowangari <verowangari34@gmail.com>
2022-04-21 18:14:21 +0300

    create task model

--- todo/settings.py
+++ todo/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'App'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,10 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todo',
+        'USER': 'moringa',
+        'PASSWORD':'Access',
     }
 }
 
