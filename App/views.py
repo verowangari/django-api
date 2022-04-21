e3222644104e0dc3e018a07ad814f6072b9eb8ca
verowangari <verowangari34@gmail.com>
2022-04-21 21:39:21 +0300

    add delete method

--- App/views.py
+++ App/views.py
@@ -48,3 +48,12 @@ def taskUpdate(request,pk):
     if serializer.is_valid():
         serializer.save()
     return Response(serializer.data)
+
+@api_view(['DELETE'])
+def taskDelete(request,pk):
+    task=Task.objects.get(id=pk)
+    task.delete()
+    
+    
+    
+    return Response('Item successfully deleted!')
