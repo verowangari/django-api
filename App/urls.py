979c77b55f9ada9a1a7469fe832cbe4a6a57ad66
verowangari <verowangari34@gmail.com>
2022-04-21 21:24:54 +0300

    create view for create and updte task

--- App/urls.py
+++ App/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns=[
     path('',views.apiOverview, name='api-overview'),
-    path('task-list/',views.taskList, name='task-list')
+    path('task-list/',views.taskList, name='task-list'),
+    path('task-detail/<str:pk>/',views.taskDetail, name='task-detail'),
+    path('task-create/',views.taskCreate, name='task-create'),
+    path('task-update/',views.taskUpdate, name='task-update'),
 ]
--- App/views.py
+++ App/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
  
 from .serializers import TaskSerializer
 from .models import Task
+from App import serializers
 
 
 # Create your views here.
@@ -15,9 +16,7 @@ def apiOverview(request):
         'Detail View':'/task-detail/<str:pk>/',
         'Create':'/task-create',
         'Update':'/task-update/<str:pk>/',
-        'Delete':'/task-delete/<str:pk>/',
-        
-        
+        'Delete':'/task-delete/<str:pk>/',    
     }
     return Response(api_urls)
 @api_view(['GET'])
@@ -25,4 +24,27 @@ def taskList(request):
     tasks=Task.objects.all()
     serializer=TaskSerializer(tasks,many=True)
     return Response(serializer.data)
+
+@api_view(['GET'])
+def taskDetail(request,pk):
+    tasks=Task.objects.get(id=pk)
+    serializer=TaskSerializer(tasks,many=False)
+    return Response(serializer.data)
     
+    
+@api_view(['POST'])
+def taskCreate(request):
+    serializer=TaskSerializer(data=request.data)
+    
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def taskUpdate(request,pk):
+    task=Task.objects.get(id=pk)
+    serializer=TaskSerializer(instance=task, data=request.data)
+    
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
