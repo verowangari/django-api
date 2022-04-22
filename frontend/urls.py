333ca4292e8045b2f1a3a5e6f8856752f3149f0a
verowangari <verowangari34@gmail.com>
2022-04-22 10:50:20 +0300

    login and signuphtml views

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,5 +1,10 @@
 from . import views
 from django.urls import path
+from django.contrib.auth import views as authViews
+from frontend.views import Signup
 urlpatterns=[
     path('',views.list,name="list"),
+    path('login/', authViews.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', authViews.LogoutView.as_view(), {'next_page' : 'index'}, name='logout'),
+    path('signup/', Signup, name='signup'),
 ]
